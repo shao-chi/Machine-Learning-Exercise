b645121d3bccd88f06de1d7d556b1df592694e7f
shao-chi <shaochi1225@gmail.com>
2019-10-02 16:58:30 +0800

    Regularization

--- WithoutLib/model/regression.py
+++ WithoutLib/model/regression.py
@@ -2,6 +2,7 @@
 from __future__ import print_function, division
 import numpy as np
 import math
+from ..utils.normalize import normalization
 
 def LinearRegression(X, Y, n_iter=100, learn_rate=0.001):
     """
@@ -38,3 +39,37 @@ def LinearRegression(X, Y, n_iter=100, learn_rate=0.001):
         weight -= grad_w * learn_rate
 
     return training_error, weight
+
+def Regularization_L1(alpha, w, grad=False):
+    """
+    L1 Penalty: λ∑|βj|
+    λ: tuninig parameter (λ → ∞ 時，懲罰效果最大，迫使所有係數都趨近於0)
+    """
+    if grad == False:
+        return alpha * np.linalg.norm(w) # 平方總和開根號 -> n維歐幾里德空間的直覺長度
+    else:
+        return alpha * np.sign(w)
+
+def Regularization_L2(alpha, w, grad=False):
+    """
+    L2 Penalty: λ∑βj^2
+    """
+    if grad == False:
+        return alpha * 0.5 * w.T.dot(w) # W^T * W
+    else:
+        return alpha * w
+
+def LassoRegression(X, Y, degree, reg_alpha, n_iter=3000, learn_rate=0.001):
+    """
+    Least absolute shrinkage and selection operator (Lasso)
+    Regression with L1 Penalty
+
+    degree: the degree of polynomial
+    reg_alpha: the amount og regularization (feature shrinkage)
+    """
+    
+
+def RidgeRegression():
+    """
+    Regression with L2 Penalty
+    """
