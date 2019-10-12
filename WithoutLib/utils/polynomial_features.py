544cfcd0679003b1a2cf533cc17ec5a8921a509e
shao-chi <shaochi1225@gmail.com>
2019-10-12 22:49:25 +0800

    PolynomialFeatures_Completed

--- WithoutLib/utils/polynomial_features.py
+++ WithoutLib/utils/polynomial_features.py
@@ -8,3 +8,17 @@ def polynomial_features(X, degree):
     the degree-2 polynomial features are [1, a, b, a^2, ab, b^2].
     """
     n_samples, n_features = np.shape(X)
+
+    def index_combinations():
+        combs = [combinations_with_replacement(range(n_features), i) for i in range(0, degree + 1)]
+        flat_combs = [item for sublist in combs for item in sublist]
+        return flat_combs
+    
+    combinations = index_combinations()
+    n_output_features = len(combinations)
+    X_new = np.empty((n_samples, n_output_features))
+    
+    for i, index_combs in enumerate(combinations):  
+        X_new[:, i] = np.prod(X[:, index_combs], axis=1)
+
+    return X_new
